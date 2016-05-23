92bfee344e5eaa03ff104a6457c22bd5dc48cdf9
eric <eric.gibert@yahoo.fr>
2016-05-23 21:23:11 +0800

    AM2302

--- ponicwatch/ponicwatch.py
+++ ponicwatch/ponicwatch.py
@@ -6,7 +6,9 @@
 """
 import argparse
 import os.path
+from croniter import croniter
 from datetime import datetime
+from time import sleep
 from model.pw_db import Ponicwatch_db
 from model.pw_log import Ponicwatch_log
 from model.user import User
@@ -29,27 +31,35 @@ class Controller(object):
         self.log = Ponicwatch_log(self.db, debug=DEBUG)
         self.log["controller_name"] = self.name
 
-        # create the sensors and their supporting hardware
-        self.sensors = {}  # key: hardware chip, values: sensors object containing the values
+        # create the sensors and their supporting hardware as a dictionary
+        #   - key: hardware chip
+        #   - value: (hardware driver, list of sensors object containing associated tothe harware)
+        self.sensors = {}
+        now = datetime.now()
         for sensor in db_sensor.list_sensors(self.db):
-            # split hardware in components
+            # split the 'hardware' text field in its components
             hw_components = sensor["hardware"].split('.')
             hw_id = hw_components[0] + '.' + hw_components[1]
-
-            if hw_id in self.sensors: # hardware has been already defined --> just add the new sensor to the list
+            new_sensor = None
+            if hw_id in self.sensors: # if the hardware has been already defined --> just add the new sensor to its list
                 (_h, _l) = self.sensors[hw_id]
                 if hw_components[0] in ["DHT11", "DHT22", "AM2302"]:
-                    sensor_dht = Sensor_DHT(_h, sensor)
-                    _l.append(sensor_dht)
+                    new_sensor = Sensor_DHT(_h, sensor)
+                    _l.append(new_sensor)
             else: # a new hardware needs to be created then that sensor starts its list
                 if hw_components[0] in ["DHT11", "DHT22", "AM2302"]:
                     assert(len(hw_components) == 3)
-                    hw_dht = Hardware_DHT(hw_components[0], hw_components[1])  # model and pin number
-                    sensor_dht = Sensor_DHT(hw_dht, sensor)
-                    self.sensors[hw_id] = ( hw_dht,[ sensor_dht ])  # the hardware object and a list of sensors
+                    hw_dht = Hardware_DHT(hw_components[0], hw_components[1])  # model and pin number foe the deriver
+                    new_sensor = Sensor_DHT(hw_dht, sensor)
+                    self.sensors[hw_id] = ( hw_dht,[ new_sensor ])  # store the hardware object and a singleton sensor
                 else:
                     print("ERROR: unknown hardware in sensor table:", hw_components[0])
                     print(sensor)
+
+            # when do we need to read the sensor?
+            if new_sensor:
+                new_sensor.cron = croniter(sensor["timer"], now)
+                new_sensor.next_read = new_sensor.cron.get_next(datetime).strftime("%Y-%m-%d %H:%M")
         # print(self.sensors)
 
 
@@ -57,16 +67,27 @@ class Controller(object):
         self.log.add_info("Controller is now running")
         self.running = True
 
-        for hw, sensor_list in self.sensors.values():
-            hw.read()
-            for sensor in sensor_list:
-                sensor.calculate_value()
-                print(sensor.name, sensor.calculated_value)
-                sensor.db_rec["calculated_value"] = sensor.calculated_value
-                self.log.add_log(self.name, system_name="Horizon 1", param=sensor.db_rec)
+        while self.running:
+            now = datetime.now().strftime("%Y-%m-%d %H:%M")  # should we take local time or UTC?
+            for hw, sensor_list in self.sensors.values():
+                # is there a sensor that needs to be read now?
+                for sensor in sensor_list:
+                    if sensor.next_read == now:
+                        hw.read()
+                        break
+                # ok, let's get the sensors reading from their hardware and set them for their next reading time
+                for sensor in sensor_list:
+                    if sensor.next_read == now:
+                        sensor.calculate_value()
+                        print(sensor.name, sensor.calculated_value)
+                        sensor.db_rec["calculated_value"] = sensor.calculated_value
+                        self.log.add_log(self.name, system_name="Horizon 1", param=sensor.db_rec)
+                        sensor.next_read = sensor.cron.get_next(datetime).strftime("%Y-%m-%d %H:%M")
 
-        # while self.running:
-        #     now = datetime.now()  # should we take local time or UTC?
+            while now == datetime.now().strftime("%Y-%m-%d %H:%M"):
+                sleep(10)
+
+
 
 
 
