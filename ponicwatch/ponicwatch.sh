82b22225bf4f19381efc437734eda438df7c714f
ericgibert@yahoo.fr <ericgibert@yahoo.fr>
2018-01-30 21:13:57 +0800

    from xcj

--- ponicwatch/ponicwatch.sh
+++ ponicwatch/ponicwatch.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
+### BEGIN INIT INFO
+# Provides:          ponicwatch.sh
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start ponicwatch daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+# https://www.dexterindustries.com/howto/run-a-program-on-your-raspberry-pi-at-startup/
 # to be executed in the same durectory as ponicwatch.py i.e. the executable part of the project
-CWD=$(dirname $(readlink -f $BASH_SOURCE))
-_IP=$(hostname -I | awk '{print $1}')
+#CWD=$(dirname $(readlink -f $BASH_SOURCE))
+CWD=/home/pi/ponicwatch/ponicwatch
+#_IP=$(hostname -I | awk '{print $1}')
+_IP=10.0.10.218
 rc=0
-PID=$(cat "$CWD"/ponicwatch.pid)
+PID=$(cat $CWD/ponicwatch.pid)
 # See how we were called.
 case "$1" in
 start)
-    export WORKON_HOME=$HOME/.virtualenvs
+    export WORKON_HOME=/home/pi/.virtualenvs
     source $WORKON_HOME/ponicwatch/bin/activate
 
     sudo pigpiod
 
-    cd "$CWD"
-    python ponicwatch.py -s local_ponicwatch.db -d2 -b $_IP > "$CWD"/../Private/ponicwatch.log 2>&1 &
+    cd $CWD
+    /home/pi/.virtualenvs/ponicwatch/bin/python $CWD/ponicwatch.py -s $CWD/local_ponicwatch.db -d2 -b $_IP > $CWD/../Private/ponicwatch.log 2>&1 &
     echo http://$_IP:8080
     ;;
 stop)
